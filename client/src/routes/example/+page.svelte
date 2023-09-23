bda70ee02c72629f18be023088001542f39d550a
dmitriy.kukuev <dmitriy.kukuev@sibirix.ru>
2023-09-23 17:48:03 +0700

    + sveltekit

---
+++ client/src/routes/example/+page.svelte
@@ -0,0 +1,55 @@
+<script lang="ts">
+    let list = [
+        {
+            id: 1,
+            name: 'todo 1',
+            checked: false,
+        },
+        {
+            id: 2,
+            name: 'todo 2',
+            checked: false,
+        },
+    ];
+
+    let newItem = '';
+
+    function addItem() {
+        if (!newItem) return;
+
+        const newId = (list.at(-1)?.id ?? 0) + 1;
+
+        list.push({
+            id: newId,
+            name: newItem,
+            checked: false,
+        })
+
+        newItem = '';
+
+        list = list;
+    }
+</script>
+
+<div>
+    <h1>example</h1>
+
+    <ul>
+        {#each list as item (item.id)}
+            <li>
+                <span>{item.name}</span>
+                <span>
+                    {item.checked ? 'done' : 'incomplete'}
+                </span>
+                <button on:click={() => item.checked = !item.checked}>
+                    {item.checked ? 'check' : 'uncheck'}
+                </button>
+            </li>
+        {/each}
+    </ul>
+
+    <input type="text" bind:value={newItem}>
+    <button on:click={addItem}>
+        add item
+    </button>
+</div>
